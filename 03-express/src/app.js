1d421cad5f730a0bf773863d6ee102b4049b90a9
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-05-11 20:01:45 -0300

    feat: add functions express

--- 03-express/src/app.js
+++ 03-express/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const app = express();
+
 const port = 3000;
 
-const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/:name?", (req, res) => {
   const { name } = req.params;
