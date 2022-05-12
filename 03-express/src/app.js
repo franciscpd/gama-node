6af8f30ab9139d048d31cabddc1ad16672550a61
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-05-11 21:06:39 -0300

    feat: add put and delete route

--- 03-express/src/app.js
+++ 03-express/src/app.js
@@ -7,6 +7,37 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/products", (req, res) => {
+  res.json([]);
+});
+
+app.post("/products", (req, res) => {
+  res.json(req.body);
+});
+
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name: `Produto ${id}`,
+    price: 300,
+  });
+});
+
+app.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    ...(req.body || {}),
+  });
+});
+
+app.delete("/products/:id", (req, res) => {
+  res.status(204).send("");
+});
+
 app.get("/:name?", (req, res) => {
   const { name } = req.params;
 
