5718dcffee6cfa1d75021d188c97bac5b5572b4b
Francisross Soares de Oliveira <franciscpd@gmail.com>
2022-05-14 09:49:37 -0300

    feat: implements router and controllers

--- 03-express/src/app.js
+++ 03-express/src/app.js
@@ -1,48 +1,14 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get("/products", (req, res) => {
-  res.json([]);
-});
-
-app.post("/products", (req, res) => {
-  res.json(req.body);
-});
-
-app.get("/products/:id", (req, res) => {
-  const { id } = req.params;
-
-  res.json({
-    id,
-    name: `Produto ${id}`,
-    price: 300,
-  });
-});
-
-app.put("/products/:id", (req, res) => {
-  const { id } = req.params;
-
-  res.json({
-    id,
-    ...(req.body || {}),
-  });
-});
-
-app.delete("/products/:id", (req, res) => {
-  res.status(204).send("");
-});
-
-app.get("/:name?", (req, res) => {
-  const { name } = req.params;
-
-  res.send(`Hello ${name || "world"}`);
-});
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Servidor executando na porta: ${port}`);
